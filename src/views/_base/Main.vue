f2521aad429f70b3b2024b9097af35ae68ee94c7
Promise-Cheng <737362053@qq.com>
2020-10-22 21:27:47 +0800

    基础框架

---
+++ src/views/_base/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :style="{overflow: 'auto',height: height + 'px'}">
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Main",
+    data(){
+      return {
+        height: window.innerHeight - 100,
+        isRouterAlive: true,
+        keepAliveList: [],
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
